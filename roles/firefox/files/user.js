3ea8e1d7353d475a51c7a49d8844f4d1ae9fb2aa
ywngr <ywngrr@gmail.com>
2026-01-20 12:50:27 +0400

    up

--- roles/firefox/files/user.js
+++ roles/firefox/files/user.js
@@ -10,3 +10,4 @@ user_pref("browser.sessionstore.max_tabs_undo", 0);
 user_pref("findbar.highlightAll", true);
 user_pref("sidebar.revamp", true);
 user_pref("sidebar.verticalTabs", true);
+user_pref("browser.download.dir", "/home/ywngr/archive/Downloads");
